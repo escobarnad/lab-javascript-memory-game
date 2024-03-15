4f4e6461dfab86e4eaa889d8c7aa5c5d07caef8b
Nadia Escobar <escobarnad@gmail.com>
2024-03-15 18:23:32 +0100

    Clean

--- src/memory.js
+++ src/memory.js
@@ -2,45 +2,21 @@ class MemoryGame {
   constructor(cards) {
     this.cards = cards;
     // add the rest of the class properties here
-    this.pickedCards = [];
-    this.pairsClicked = 0;
-    this.pairsGuessed = 0;
+
   }
 
   shuffleCards() {
     // ... write your code here
-    if (!this.cards) {
-      return undefined
-    }
 
-    for (let i = 0; i<this.cards.length -1; i += 1) {
-      let shuffle = Math.floor(Math.random () *(this.cards.length -1) +1)
-      let temp = this.cards[i]
-      this.cards[i] = this.cards[shuffle]
-      this.cards[shuffle] = temp
-    }
   }
 
   checkIfPair(card1, card2) {
     // ... write your code here
-    this.pairsClicked += 1
-    if (card1 === card2){
-      this.pairsGuessed += 1
-      return true
-    }
-    else {
-      return false
-    }
+
   }
 
   checkIfFinished() {
     // ... write your code here
 
-    if (this.pairsGuessed === this.cards.length/2){
-      return true
-    }
-    else {
-      return false
-    }
   }
 }
